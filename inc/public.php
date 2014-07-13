02e9ecd6e85aa8ecac47500aae870dd272fccb45
Kees de Bruin <halfje.bruin@gmail.com>
2014-07-13 14:42:49 +0200

    Added initial [hb-image] shortcode implementation

--- inc/public.php
+++ inc/public.php
@@ -4,9 +4,20 @@ class HBPublic extends HBPlugin
 
 	protected function init()
 	{
+		$this->add_shortcode('hb-image', 'hb_image_shortcode');
 	}
 
 	function hb_image_shortcode($atts, $content = null, $code = "")
 	{
+		extract(shortcode_atts(array(
+				'id' => null,
+				'size' => 'thumbnail',
+				'align' => 'none'
+		), $atts));
+		
+		if (empty($id)) return;
+
+		$html = get_image_tag($id, '', '', $align, $size);
+		echo $html;
 	}
 }
